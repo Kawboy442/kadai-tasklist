fc5f5a355b8541a7cc9571382cb7561801439486
ryosuke.ito <kawboy442@gmail.com>
2021-01-19 21:51:40 +0900

    feat: Hibernateで必要な処理と設定を追加

--- src/models/task.java
+++ src/models/task.java
@@ -7,9 +7,21 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 @Entity
+@NamedQueries({
+    @NamedQuery(
+        name = "getAllTasks",
+        query = "SELECT m FROM Task AS m ORDER BY m.id DESC"
+    ),
+    @NamedQuery(
+        name = "getTasksCount",
+        query = "SELECT COUNT(m) FROM Task AS m"
+    )
+})
 @Table(name = "tasks")
 public class Task {
     @Id
@@ -17,7 +29,7 @@ public class Task {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(name = "content", length = 255, nullable = false)
+    @Column(name = "content", nullable = false)
     private String content;
 
     @Column(name = "created_at", nullable = false)
